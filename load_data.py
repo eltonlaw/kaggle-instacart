6d2539ddcfccc47bfc582b74cce8c0d950ede249
EltonLaw <eltonlaw296@gmail.com>
2017-07-31 23:55:48 -0400

    add test set loading and train set labels to batch loader

--- load_data.py
+++ load_data.py
@@ -39,29 +39,64 @@ class Instacart:
                                         'aisle_id': np.uint8,
                                         'department_id': np.uint8
                                     })
-        # Get all unique user id's in train
-        self.user_ids = self.train["order_id"].unique()
+
         # Number of labels/products/categories
         self.n_cats = np.shape(self.products)[0]
-        # Number of unique user id's in train/training datapoints
-        self.n_users = np.shape(self.user_ids)[0]
         self.batch_counter = 0
 
     def next_batch(self):
-        """ Load batches"""
+        """ Load training batches"""
+        # Get all user id's in train/test
+        user_ids = self.orders[self.orders.eval_set == "train"].user_id
+        # Number of unique user id's in train datapoints
+        n_users = np.shape(user_ids)[0]
         # Initialize sparse data matrix
-        data = np.zeros((self.batch_size, self.n_cats))
+        X = np.zeros((self.batch_size, self.n_cats))
+        y = np.zeros((self.batch_size, self.n_cats))
 
         bs = self.batch_size
         bc = self.batch_counter
         # For each user in the training set get all order ids
-        for ii, uid in enumerate(self.user_ids[bc*bs:(bc+1)*bs]):
-            oids = self.orders.loc[self.orders['user_id'] == uid]["order_id"]
-
+        for ii, uid in enumerate(user_ids[bc*bs:(bc+1)*bs]):
             # For each order_id get all product_id's and add to sparse matrix
-            for oid in oids.values:
-                pids = self.priors[self.priors["order_id"] == oid]["product_id"]
+            oids_tr = self.orders.loc[(self.orders.user_id == uid) &
+                                      (self.orders.eval_set == "train")]
+            oids_pr = self.orders.loc[(self.orders.user_id == uid) &
+                                      (self.orders.eval_set == "prior")]
+            # If order_id is in 'prior', add to data
+            for oid in oids_pr.order_id.values:
+                pids = self.priors[self.priors.order_id == oid].product_id
                 for pid in pids.values:
-                    data[ii, pid] += 1
+                    X[ii, pid] += 1
+            # If order_id is in 'train', add to labels
+            oid = oids_tr.order_id.values[0]
+            pids = self.priors[self.priors.order_id == oid].product_id
+            for pid in pids.values:
+                y[ii, pid] += 1
         self.batch_counter += 1
-        return data
+        if self.batch_counter == int(n_users/self.batch_size):
+            self.batch_counter = 0
+        return X, y
+
+    def load_test(self):
+        """ Load test set """
+        # Get all user id's in train/test
+        user_ids = self.orders[self.orders.eval_set == "test"].user_id[:10]
+        # Number of unique user id's in train datapoints
+        n_users = np.shape(user_ids)[0]
+
+        X = np.zeros((n_users, self.n_cats))
+        oids_to_predict = []
+        for ii, uid in enumerate(user_ids):
+            # For each order_id get all product_id's and add to sparse matrix
+            oids_te = self.orders.loc[(self.orders.user_id == uid) &
+                                      (self.orders.eval_set == "test")]
+            oids_pr = self.orders.loc[(self.orders.user_id == uid) &
+                                      (self.orders.eval_set == "prior")]
+            for oid in oids_pr.order_id.values:
+                # If order_id is in 'prior', add to data
+                pids = self.priors[self.priors.order_id == oid].product_id
+                for pid in pids.values:
+                    X[ii, pid] += 1
+            oids_to_predict.append(oids_te.order_id.values[0])
+        return X, oids_to_predict
